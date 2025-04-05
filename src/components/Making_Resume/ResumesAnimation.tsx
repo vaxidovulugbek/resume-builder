0bb63d9484f3c1b7fba48a29e5ca6abbc45eaeaa
ulugbek <vulugb@gmail.com>
2025-04-05 20:43:20 +0500

    fix

--- src/components/Making_Resume/ResumesAnimation.tsx
+++ src/components/Making_Resume/ResumesAnimation.tsx
@@ -1,52 +1,58 @@
 import React from "react";
 import "./Making_Resume.scss";
+import res1 from "assets/imgs/resume_templates/res1.jpg";
+import res3 from "assets/imgs/resume_templates/res3.jpg";
+import res5 from "assets/imgs/resume_templates/res5.jpg";
+import res4 from "assets/imgs/resume_templates/res4.jpg";
+import res2 from "assets/imgs/resume_templates/res2.jpg";
+import res6 from "assets/imgs/resume_templates/res6.jpg";
 
 const Animation: React.FC<{ className: string; num: string }> = ({ className, num }) => {
 	return (
 		<div className={`slider ${className}`}>
 			<div className={`${num === "1" ? "slide-track" : "slide-track-Bottom"}`}>
 				<div className="slide">
-					<img src="https://marketplace.canva.com/EAFC-9sdKHg/2/0/1131w/canva-black-and-white-minimalist-simple-design-freelancer-resume-wIpoHMlKB6I.jpg" />
+					<img src={res1} />
 				</div>
 				<div className="slide">
-					<img src="https://marketplace.canva.com/EAEoLAxIWIo/1/0/1131w/canva-black-and-white-color-blocks-software-engineer-resume--RKK1cTL5JA.jpg" />
+					<img src={res2} />
 				</div>
 				<div className="slide">
-					<img src="https://cdn-images.zety.com/templates/zety/minimo-4-classic-blue-navy-200@3x.png" />
+					<img src={res3} />
 				</div>
 				<div className="slide">
-					<img src="https://resumegenius.com/wp-content/uploads/2024-Modern-Resume-Template-Violet.png" />
+					<img src={res4} />
 				</div>
 				<div className="slide">
-					<img src="https://i.etsystatic.com/43852575/r/il/e184e7/4982637963/il_fullxfull.4982637963_byt7.jpg" />
+					<img src={res5} />
+				</div>
+				<div className="slide">
+					<img src={res6} />
 				</div>
 				<div className="slide">
 					<img src="https://resumegenius.com/wp-content/uploads/clean-resume-skin-orange.png" />
-				</div>
-				<div className="slide">
-					<img src="https://i.pinimg.com/736x/82/e0/92/82e092be16cf77305cbfff242ccdb7e9.jpg" />
 				</div>
 				{/* end */}
 				<div className="slide">
-					<img src="https://marketplace.canva.com/EAFC-9sdKHg/2/0/1131w/canva-black-and-white-minimalist-simple-design-freelancer-resume-wIpoHMlKB6I.jpg" />
+					<img src={res1} />
 				</div>
 				<div className="slide">
-					<img src="https://marketplace.canva.com/EAEoLAxIWIo/1/0/1131w/canva-black-and-white-color-blocks-software-engineer-resume--RKK1cTL5JA.jpg" />
+					<img src={res2} />
 				</div>
 				<div className="slide">
-					<img src="https://cdn-images.zety.com/templates/zety/minimo-4-classic-blue-navy-200@3x.png" />
+					<img src={res3} />
 				</div>
 				<div className="slide">
-					<img src="https://resumegenius.com/wp-content/uploads/2024-Modern-Resume-Template-Violet.png" />
+					<img src={res4} />
 				</div>
 				<div className="slide">
-					<img src="https://i.etsystatic.com/43852575/r/il/e184e7/4982637963/il_fullxfull.4982637963_byt7.jpg" />
+					<img src={res5} />
+				</div>
+				<div className="slide">
+					<img src={res6} />
 				</div>
 				<div className="slide">
 					<img src="https://resumegenius.com/wp-content/uploads/clean-resume-skin-orange.png" />
-				</div>
-				<div className="slide">
-					<img src="https://i.pinimg.com/736x/82/e0/92/82e092be16cf77305cbfff242ccdb7e9.jpg" />
 				</div>
 			</div>
 		</div>
